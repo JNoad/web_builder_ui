208372426183a19b2f99d46c4109126b49094e5e
John-Bruce Noad <noadjohnbruce@gmail.com>
2024-09-08 21:01:08 +0200

    Home Page Layout

---
+++ src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="side-bar-container">
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    .side-bar-container {
+        width: 300px; height: 100%;
+        border: 2px solid black !important;
+    }
+</style>
