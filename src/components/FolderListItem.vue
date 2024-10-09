6b9431870bbe8803024c69897d880b78b699961f
= <=>
2024-10-09 18:43:54 +0200

    Folder List Item

---
+++ src/components/FolderListItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <tr>
+        <td>
+            <div>
+                <span class="material-symbols-outlined folder-icon" :style="`color: ${folderColour ? folderColour : '#565656'};`">
+                    folder
+                </span>
+                <span>{{ folderName }}</span>
+            </div>
+        </td>
+        <td>{{ owner }}</td>
+        <td>{{ [new Date(updatedAt).toLocaleString('default', {day: 'numeric', month: 'short', year: 'numeric'})].join(' ') }}</td>
+        <td>{{ size > 1000 ? (repo.size / 1024).toFixed(2) + "mb" : size + "kb" }}</td>
+        <td>:</td>
+    </tr>
+</template>
+<script>
+export default {
+    props: ['folderName', 'owner', 'updatedAt', 'size', 'folderColour'],
+
+}
+</script>
+<style lang="scss" scoped>
+    tr {
+
+        &:hover {
+            background-color: #eee;
+        }
+        td {
+            cursor: default;
+            div {
+                @include flex-row();
+                span {
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
+</style>
